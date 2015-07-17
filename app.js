953f01a7ad57368ce30c03e2fb59efc3e28cf80c
kimihito <tatsurotamashiro@gmail.com>
2015-07-17 18:25:18 +0900

    init

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict'
+
+function clicked() {
+  console.log('Hi!');
+};
---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict'
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+require('crash-reporter').start()
+
+var mainWindow = null
+
+app.on('window-all-closed', function(){
+  if(process.platform != 'darwin'){
+    app.quit()
+  }
+})
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({
+    width: 90,
+    height: 90,
+    frame: false,
+    transparent: true,
+    // resizable: false
+  });
+  mainWindow.loadUrl('file://' + __dirname + '/index.html')
+
+  mainWindow.on('closed', function(){
+    mainWindow = null
+  })
+})
