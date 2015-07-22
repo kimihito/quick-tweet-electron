ce4b773ab83b6cff60aa7f8e39d6193f6a20d311
kimihito <tatsurotamashiro@gmail.com>
2015-07-23 00:35:29 +0900

    wip

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict'
 var app = require('app')
 var BrowserWindow = require('browser-window')
+var Twitter = require('twitter');
+var OAuth = require('oauth').OAuth;
 
 require('crash-reporter').start()
 
@@ -13,16 +15,50 @@ app.on('window-all-closed', function(){
 })
 
 app.on('ready', function(){
-  mainWindow = new BrowserWindow({
-    width: 600,
-    height: 450,
-    frame: false,
-    transparent: true,
-    resizable: false
-  });
-  mainWindow.loadUrl('file://' + __dirname + '/index.html')
 
-  mainWindow.on('closed', function(){
-    mainWindow = null
+  // var authUrl = "https://api.twitter.com/oauth/authenticate?oauth_token=";
+  var authUrl = 'https://api.twitter.com/oauth/authenticate?oauth_token=';
+  var oauth = new OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    'D4p8XQDK5d61K3sl1BwWHHTjs',
+    'nlVkLxQXNbZutajyewVW7cDCccycHmxHZeXRPy4t5jByyGD5bi',
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  );
+
+  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+    if(error){
+      console.error(error);
+      return;
+    }
+
+    console.log('oauth_token : ' +  oauth_token);
+    console.log('oauth_token_secret : ' + oauth_token_secret);
+    var url = authUrl + oauth_token;
+    var loginWindow = new BrowserWindow({width: 800, height: 600});
+    loginWindow.webContents.on('will-navigate', function(event, url){
+      var urlinfo = require('url').parse(url, true);
+      if(urlinfo.query.oauth_verifier){
+        oauth.getOAuthAccessToken(oauth_token, oauth_token_secret, urlinfo.query.verifier, function(error, oauth_access_token, oauth_access_token_secret){
+          if(error){
+            console.error(error);
+            return;
+          }
+          console.log('oauth_access_token : ' + oauth_access_token);
+          console.log('oauth_access_token_secret : ' + oauth_access_token_secret);
+          setTimeout(function(){
+            loginWindow.close();
+          },0);
+        });
+      }else{
+        setTimeout(function(){
+          loginWindow.close();
+        },0);
+      }
+      event.preventDefault();
+    });
+    loginWindow.loadUrl(url);
   })
 })
